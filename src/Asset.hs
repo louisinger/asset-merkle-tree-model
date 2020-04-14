167aa8dead4e41e5574d638ebf35c0538adf9ae9
Louis Singer <louis.singer.git@gmail.com>
2020-04-14 21:55:39 +0200

    Asset module v0.1

--- src/Asset.hs
+++ src/Asset.hs
@@ -1,15 +1,31 @@
 module Asset where
 
+  import Data.List (groupBy, sortBy)
   import Data.ByteString (ByteString)
   import Crypto.Hash.SHA256 (hash)
-  import MerkleTree (Tree, node)
+  import MerkleTree (Tree (Leaf, Node), node, merkleRoot)
+  import Data.Ord (comparing)
 
   data Asset = Asset { 
     typeOf :: ByteString,
-    assets :: [Asset]
-  }
+    content :: [Asset]
+  } deriving (Show)
 
-  sortByType :: [Asset] -> [[Asset]]
+  sortByType :: [Asset] -> [Asset]
+  sortByType = sortBy (comparing typeOf)
+
+  groupByType :: [Asset] -> [[Asset]]
+  groupByType = groupBy (\a a' -> typeOf a == typeOf a')
+
+  sortAndGroup :: [Asset] -> [[Asset]]
+  sortAndGroup = groupByType . sortByType
+
+  typeTree :: Asset -> Tree
+  typeTree = Leaf . hash . typeOf
+
+  contentTree :: Asset -> Tree
+  contentTree a = merkleRoot [(merkleRoot . (map assetTree)) as | as <- (sortAndGroup . content) a]
 
   assetTree :: Asset -> Tree
-  assetTree a = 
+  assetTree (Asset t []) = typeTree (Asset t [])
+  assetTree a = merkleRoot [typeTree a, contentTree a]
--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,17 @@
 module Main where
 
 import MerkleTree
+import Asset
+import Data.ByteString.Char8 (pack)
+
+mockAssetList :: [Asset]
+mockAssetList = [b3, b1, b2, b1, b1, b3, b1, b2, b3] 
+  where
+    pill = Asset (pack "pill_aspirine") []
+    b1 = Asset (pack "box_paracetamol")  []
+    b2 = Asset (pack "box_morphine")  []
+    b3 = Asset (pack "box_aspirine")  [pill, pill, pill]
+
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
